b9d9305172f82ca100624d799ee6c0e0af522071
vmarjunior <valdir.mjunior@hotmail.com>
2020-03-29 14:29:40 -0300

    hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'material-demo';
+  title = "material-demo";
+  defaultGender = true;
+  genders = [
+    { name: "Male", isDefault: false },
+    { name: "Female", isDefault: false },
+    { name: "Non-Binary", isDefault: false },
+    { name: "Other", isDefault: true }
+  ];
+
+  foods = [
+    { value: 1, viewValue: "Pizza" },
+    { value: 2, viewValue: "Hamburger" },
+    { value: 3, viewValue: "Ice Cream" },
+    { value: 4, viewValue: "Chocolate Pie" }
+  ];
+
+  categories = [
+    { name: "Beginner", selected: false },
+    { name: "Intermediate", selected: false },
+    { name: "Advanced", selected: false }
+  ];
+
+  progress = 0;
+  timer;
+
+  constructor() {
+    this.timer = setInterval(() => {
+      this.progress++;
+
+      if(this.progress === 100)
+      clearInterval(this.timer);
+    }, 20);
+
+  }
+
+  selectCategory(category){
+    //Only one category will be selected
+    this.categories
+    .filter(c => c != category)
+    .forEach(c => c.selected = false);
+
+    category.selected = !category.selected;
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+//Angular Materials
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSelectModule } from "@angular/material/select";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatIconModule } from "@angular/material/icon";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
+import { NgModule } from "@angular/core";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
+
